723467d03d4e757815daa7754d5926d55f9f8d51
abdninesix <mohabd1995@gmail.com>
2025-11-28 19:56:43 +0500

    Updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useState } from 'react'
 import { HiOutlineLockClosed, HiOutlineMail, HiOutlineUser } from 'react-icons/hi'
 import RightSection from './RightSection'
+import * as z from 'zod'
+
+const schema = z.object({
+    username: z.string().min(3, 'Username must be at least 3 characters'),
+    password: z.string().min(8, 'Password must be at least 8 characters'),
+})
 
 const Login = () => {
+
+    const [error, setError] = useState({})
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const result = schema.safeParse({
+            username: e.target.username.value,
+            password: e.target.password.value,
+        });
+
+        if (!result.success) {
+            setError(result.error.issues.reduce((acc, curr) => ({ ...acc, [curr.path[0]]: curr.message, }), {}));
+            return;
+        }
+
+        setError({});
+    };
+
+
     return (
         <div className='h-screen flex p-4'>
             {/* Left section */}
@@ -12,7 +38,7 @@ const Login = () => {
                     <img src='/logo2.svg' alt='logo' />
                 </a>
                 {/* Login form */}
-                <form className='flex flex-col items-center'>
+                <form onSubmit={handleSubmit} className='flex flex-col items-center'>
                     <div className='w-1/2 flex flex-col gap-4 text-gray-600'>
 
                         <div className='space-y-2'>
@@ -26,6 +52,7 @@ const Login = () => {
                                 <HiOutlineUser className='text-xl' />
                                 <input type='text' placeholder='Enter username' id='username' className='w-full outline-none' />
                             </div>
+                            {error.username && <p className='text-red-600 text-xs'>{error.username}</p>}
                         </div>
 
                         <div>
@@ -34,12 +61,13 @@ const Login = () => {
                                 <HiOutlineLockClosed className='text-xl' />
                                 <input type='password' placeholder='Create a password' id='password' className='w-full outline-none' />
                             </div>
+                            {error.password && <p className='text-red-600 text-xs'>{error.password}</p>}
                         </div>
 
                         <button type='submit' className='bg-sky-500 hover:opacity-90 cursor-pointer text-white font-semibold py-2 rounded-lg'>Login</button>
 
                         <p className='text-center text-sm'>
-                            Don't have an account? <a href='#' className='text-sky-500'>Sign up</a>
+                            Don't have an account? <a href='/signup' className='text-sky-500'>Sign up</a>
                         </p>
                     </div>
                 </form>
@@ -57,3 +85,5 @@ const Login = () => {
 }
 
 export default Login
+
+
