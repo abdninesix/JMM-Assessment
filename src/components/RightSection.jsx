d7ab169f22a2947b29301451f5de8ca7d2b16370
abdninesix <mohabd1995@gmail.com>
2025-11-28 18:57:09 +0500

    Updated

--- src/components/RightSection.jsx
+++ src/components/RightSection.jsx
@@ -4,9 +4,7 @@ const RightSection = () => {
     return (
         <section className='w-1/2 flex flex-col items-center p-20 justify-center bg-linear-to-br from-sky-400 to-sky-950 bg- rounded-2xl'>
             {/* Design */}
-            <div>
-                Design
-            </div>
+                <img src='/content.png' alt='content' className='' />
             {/* Text */}
             <div className='space-y-4 text-center text-white'>
                 <h2 className='text-3xl font-semibold'>Transforming the Future of Resource Planning</h2>
